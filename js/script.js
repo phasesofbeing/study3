61e4d74f7662935df3816022809dff264a71a8c8
phasesofbeing <19gosha19@mail.ru>
2024-10-21 21:22:54 +0500

    praktika
    sdelal

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({ 
+        arrows:false,
+        dots:false,
+        autoplay:true,
+        slidesToShow:4,
+        slidesToScroll:1,
+        speed:500,
+        infinite:true,
+        easing:'ease',
+        autoplaySpeed: 1000,
+    });
+});
